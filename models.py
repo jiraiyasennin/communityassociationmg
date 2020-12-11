1e65ba5fa9665945051dfc3e6de31e2cfd185a0b
Dostow <d.ugel@protonmail.com>
2020-12-11 17:04:06 +0100

    Form model commit

--- models.py
+++ models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class socio(models.Model):
+    numero_de_socio = models.IntegerField()
+    nombre = models.CharField(max_length=40)
+    apellidos = models.CharField(max_length=50)
+    dni = models.CharField(max_length=15)
+    fecha_de_nacimiento = models.DateField()
+    direccion = models.CharField(max_length=200)
+    codigo_postal = models.IntegerField()
+    localidad = models.CharField(max_length=20)
+    telefono_fijo = models.IntegerField()
+    telefono_movil = models.IntegerField()
+    #Constantes del pago actual
+    PAGADO = 'SI'
+    NOPAGADO = 'NO'
+    estado_pago_actual = [(PAGADO, 'Pagado'),(NOPAGADO,'No pagado')]
+    pago_año_actual = models.CharField(max_length=2, choices=estado_pago_actual, default=NOPAGADO)
+
+    #Constantes del estatus#
+    ALTA = 'AL'
+    BAJA = 'BA'
+    estados_de_estatus = [(ALTA,'Alta'),(BAJA,'Baja')]
+    estatus = models.CharField(max_length=2, choices=estados_de_estatus, default=ALTA)
+    años_pagados = models.CharField(max_length=300)
+
+
--- views.py
+++ views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from . models import socio
+from .forms import FormularioRegistroSocio
+from django.views.generic import ListView, CreateView
+from django.contrib import messages
 
-# Create your views here.
+class sociosListView(ListView):
+    model = socio
+    template_name = 'communitymgr/socio_list.html'
+    context_object_name = 'socios'
+    pass
+    
+
+def RegistroDeSocios(request):
+    if request.method == 'POST':
+        form = FormularioRegistroSocio(request.POST)
+        if form.is_valid():
+            messages.success(request, f'The user has been created!')
+            return redirect('registro-socio')
+    else:
+        form = FormularioRegistroSocio()
+    return render(request, 'communitymgr/socio_create.html', {'form': form})
+    
